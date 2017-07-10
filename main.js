9135c4efed9626f8ac12a801c21e075eb4657e30
Simon Brynskog <simon.brynskog@istone.com>
2017-07-10 09:19:32 +0200

    commit visual stuff

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-var cards = [
+//Creating variables
+var cardsArray = [
   "H2",
   "H3",
   "H4",
@@ -63,13 +64,16 @@ var players = [
   { name: "Player 8", cardOne: "", cardTwo: "" },
   { name: "Player 9", cardOne: "", cardTwo: "" }
 ];
+var cards = cardsArray.slice();
 
+//Start of functions
 function generateRandomNumber(randomNumber) {
   randomNumber = Math.floor(Math.random() * (cards.length - 1));
   return randomNumber;
 }
 
 function dealHoleCards() {
+  
   players.forEach(function(player) {
     let randomNumber = generateRandomNumber();
     player.cardOne = cards[randomNumber];
@@ -78,8 +82,11 @@ function dealHoleCards() {
     randomNumber = generateRandomNumber();
     player.cardTwo = cards[randomNumber];
     cards.splice(randomNumber, 1);
+
+    let listItem = document.createElement('li');
+    listItem.innerHTML = '<p>Player name: ' + player.name + ' </p><p> Cards: ' + player.cardOne + '-' + player.cardTwo;
+    document.getElementById('seats').appendChild(listItem);
   });
-  console.log(players);
 }
 
 function dealFlop() {
@@ -89,19 +96,33 @@ function dealFlop() {
     flop.push(cards[randomNumber]);
     cards.splice(randomNumber, 1);
   }
-  console.log(flop);
+  let boardItem = document.createElement('li');
+  boardItem.innerHTML = '<p>' + flop + '</p>';
+  document.getElementById('board').appendChild(boardItem);
 }
 
 function dealTurn() {
   let randomNumber = generateRandomNumber();
   let turn = cards[randomNumber];
   cards.splice(randomNumber, 1);
-  console.log(turn);
+  
+  let boardItem = document.createElement('li');
+  boardItem.innerHTML = '<p>' + turn + '</p>';
+  document.getElementById('board').appendChild(boardItem);
 }
 
 function dealRiver() {
   let randomNumber = generateRandomNumber();
   let river = cards[randomNumber];
   cards.splice(randomNumber, 1);
-  console.log(river);
+
+  let boardItem = document.createElement('li');
+  boardItem.innerHTML = '<p>' + river + '</p>';
+  document.getElementById('board').appendChild(boardItem);
+}
+
+function reset() {
+  cards = cardsArray.slice();
+  document.getElementById('seats').innerHTML = '';
+  document.getElementById('board').innerHTML = '';
 }
